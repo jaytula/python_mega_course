8db44e91382b4f3c1a7793663ef03cda2f67614c
Jay T <jaytula@gmail.com>
2021-08-13 23:53:05 -0700

    13. Simple Types: Integers, Strings, and Floats

--- basics.py
+++ basics.py
@@ -7,9 +7,30 @@ def hours_in_weeks(week: int = 1):
 
   return week * week_hours
 
+def additions():
+  x = 10
+  y = '10'
+  z = 10.1
+
+  sum1 = x + x
+  sum2 = y + y
+
+  return (sum1, sum2, x, y, z)
+
 class Basics(unittest.TestCase):
   def test_hours_in_week(self):
     self.assertEqual(hours_in_weeks(), 168)
+
+  def test_additions(self):
+    sum1, sum2, x, y, z = additions()
+
+    self.assertEqual(sum1, 20)
+    self.assertEqual(sum2, '1010')
+    self.assertEqual(type(x), int)
+    self.assertEqual(type(y), str)
+    self.assertEqual(type(z), float)
+
+
 
 if __name__ == '__main__':
   unittest.main()
