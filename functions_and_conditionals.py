744a4c850022e9c8ae6b638abdbea655dfff7890
Jay T <jaytula@gmail.com>
2021-08-14 02:56:31 -0700

    Enable Flake8 linting

--- functions_and_conditionals.py
+++ functions_and_conditionals.py
@@ -1,18 +1,22 @@
 from typing import List
 import unittest
 
+
 def mean(numbers: List[int]) -> float:
-  return sum(numbers)/len(numbers)
+    return sum(numbers)/len(numbers)
+
 
 def just_pass():
-  pass
+    pass
+
 
 class FunctionsAndConditionals(unittest.TestCase):
-  def test_creating_functions(self):
-    self.assertAlmostEqual(mean([3, 4, 5, 6]), 4.5)
+    def test_creating_functions(self):
+        self.assertAlmostEqual(mean([3, 4, 5, 6]), 4.5)
 
-  def test_function_that_has_no_reutrn(self):
-    self.assertEqual(just_pass(), None)
+    def test_function_that_has_no_reutrn(self):
+        self.assertEqual(just_pass(), None)
+
 
 if __name__ == '__main__':
-  unittest.main()
+    unittest.main()
