01a5d0149b0c5476aea46aa1b4de95e50517b8b8
Jay T <jaytula@gmail.com>
2021-08-14 02:38:23 -0700

    38. Print or Return?

--- functions_and_conditionals.py
+++ functions_and_conditionals.py
@@ -4,9 +4,15 @@ import unittest
 def mean(numbers: List[int]) -> float:
   return sum(numbers)/len(numbers)
 
+def just_pass():
+  pass
+
 class FunctionsAndConditionals(unittest.TestCase):
   def test_creating_functions(self):
     self.assertAlmostEqual(mean([3, 4, 5, 6]), 4.5)
+
+  def test_function_that_has_no_reutrn(self):
+    self.assertEqual(just_pass(), None)
 
 if __name__ == '__main__':
   unittest.main()
