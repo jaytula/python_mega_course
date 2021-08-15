83380df6b10f9b626c290b95563eb1a2f06b0a81
Jay T <jaytula@gmail.com>
2021-08-15 09:56:50 -0700

    49. String Formatting with Multiple Variables

--- string_formatting.py
+++ string_formatting.py
@@ -1,4 +1,13 @@
 if __name__ == '__main__':
-  your_name = 'Ardit'
-  print(f'Hello {your_name}!')
-  print("Hello %s!" % your_name) # Since Python 3.6
+  name = 'Ardit'
+  surname = 'Sulce'
+  when = "today"
+
+  print(f'Hello {name}!')
+  print("Hello %s!" % name) # Since Python 3.6
+
+  message =  "Hello %s %s" % (name, surname)
+  print(message)
+  
+  message = f"Hello {name} {surname}. What's up {when}"
+  print(message)
