d57b3d403420e50e5f13162786e8752ebda7e7f6
Jay T <jaytula@gmail.com>
2021-08-15 01:27:03 -0700

    Adjust back to regular division

--- functions_and_conditionals.py
+++ functions_and_conditionals.py
@@ -2,12 +2,12 @@ from typing import List, Dict, Union
 import unittest
 
 
-def mean(value: Union[List[int], Dict[str, float]]) -> int:
+def mean(value: Union[List[int], Dict[str, float]]) -> float:
     if isinstance(value, list):
-        return sum(value) // len(value)
+        return sum(value) / len(value)
     
     dict_values = value.values()
-    return int(sum(dict_values) // len(dict_values))
+    return sum(dict_values) / len(dict_values)
 
 
 def just_pass():
@@ -16,9 +16,9 @@ def just_pass():
 
 class FunctionsAndConditionals(unittest.TestCase):
     def test_creating_functions(self):
-        self.assertAlmostEqual(mean([3, 4, 5, 6]), 4)
+        self.assertAlmostEqual(mean([3, 4, 5, 6]), 4.5)
         student_grades = {"Marry": 9.1, "Sim": 8.8, "John": 7.5}
-        self.assertAlmostEqual(mean(student_grades), 8, 2)
+        self.assertAlmostEqual(mean(student_grades), 8.47, 2)
 
     def test_function_that_has_no_return(self):
         self.assertEqual(just_pass(), None)
