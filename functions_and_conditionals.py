ddd6a64c5b883d0ae0764308ec58e8fbdc098922
Jay T <jaytula@gmail.com>
2021-08-15 00:50:38 -0700

    40. If Conditional Example

--- functions_and_conditionals.py
+++ functions_and_conditionals.py
@@ -1,9 +1,13 @@
-from typing import List
+from typing import List, Dict, Union
 import unittest
 
 
-def mean(numbers: List[int]) -> float:
-    return sum(numbers)/len(numbers)
+def mean(value: Union[List[int], Dict[str, float]]) -> float:
+    if type(value) == list:
+        return sum(value)/len(value)
+    if type(value) == dict:
+        dict_values = value.values()
+        return sum(dict_values) / len(dict_values)
 
 
 def just_pass():
@@ -13,8 +17,10 @@ def just_pass():
 class FunctionsAndConditionals(unittest.TestCase):
     def test_creating_functions(self):
         self.assertAlmostEqual(mean([3, 4, 5, 6]), 4.5)
+        student_grades = {"Marry": 9.1, "Sim": 8.8, "John": 7.5}
+        self.assertAlmostEqual(mean(student_grades), 8.47, 2)
 
-    def test_function_that_has_no_reutrn(self):
+    def test_function_that_has_no_return(self):
         self.assertEqual(just_pass(), None)
 
 
