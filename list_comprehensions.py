511c6c1e8755ddc24fbef41cb60d6687bbde559d
Jay T <jaytula@gmail.com>
2021-08-16 14:12:21 -0700

    Coding Exercise 36: Only Numbers

--- list_comprehensions.py
+++ list_comprehensions.py
@@ -1,3 +1,4 @@
+from typing import List, Union
 import unittest
 
 
@@ -13,6 +14,13 @@ class MyTestCase(unittest.TestCase):
 
     result = [temp / 10 for temp in temps if temp != -9999]
     self.assertEqual(result, [22.1, 23.4, 34.0, 23.0])
+
+  def test_exercise_36(self):
+    def foo(items: List[Union[int, str]]):
+      return [item for item in items if isinstance(item, int)]
+
+    items = [99, 'no data', 95, 94, 'no data']
+    self.assertEqual(foo(items), [99, 95, 94])
 
 if __name__ == '__main__':
   unittest.main()
