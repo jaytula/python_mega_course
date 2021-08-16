f58314aa378f595e3838194472a49fb86a43ee3b
Jay T <jaytula@gmail.com>
2021-08-16 14:06:41 -0700

    66. List Comprehension with if Conditional

--- list_comprehensions.py
+++ list_comprehensions.py
@@ -1,10 +1,17 @@
 import unittest
 
-temps = [221, 234, 340, 230]
 
 class MyTestCase(unittest.TestCase):
   def test_simple_list_comprehensions(self):
+    temps = [221, 234, 340, 230]
+
     result = [temp / 10 for temp in temps]
+    self.assertEqual(result, [22.1, 23.4, 34.0, 23.0])
+
+  def test_list_comprehension_with_conditional(self):
+    temps = [221, 234, 340, -9999, 230]
+
+    result = [temp / 10 for temp in temps if temp != -9999]
     self.assertEqual(result, [22.1, 23.4, 34.0, 23.0])
 
 if __name__ == '__main__':
