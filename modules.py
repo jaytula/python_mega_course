2e4247a97c1ab1742d2197fbd35c4322fea71422
Jay T <jaytula@gmail.com>
2021-08-18 20:47:29 -0700

    85. Standard Python Modules

--- modules.py
+++ modules.py
@@ -1,6 +1,7 @@
 import unittest
 import sys
 import time
+import os
 
 class MyTestCase(unittest.TestCase):
   def test_builtin_modules(self):
@@ -8,8 +9,15 @@ class MyTestCase(unittest.TestCase):
     self.assertTrue('sleep' in dir(time))
 
     print('Start sleep')
-    time.sleep(2)
+    time.sleep(0.5)
     print('End timer')
+
+  def test_standard_python_modules(self):
+    self.assertTrue(os.path.exists('files/vegetables.txt'))
+    self.assertTrue(sys.prefix.endswith('.pyenv/versions/3.9.6'))
+    self.assertTrue(os.path.exists(f'{sys.prefix}/lib/python3.9/os.py'))
+    self.assertTrue('listdir' in dir(os))
+    self.assertTrue('os.py' in os.listdir(f'{sys.prefix}/lib/python3.9'))
 
 if __name__ == '__main__':
   unittest.main()
