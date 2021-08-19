e148f959c13ef1047a5f0c412e6467110d553f22
Jay T <jaytula@gmail.com>
2021-08-18 20:55:04 -0700

    86. Third-Party Modules

--- modules.py
+++ modules.py
@@ -2,6 +2,7 @@ import unittest
 import sys
 import time
 import os
+import pandas
 
 class MyTestCase(unittest.TestCase):
   def test_builtin_modules(self):
@@ -18,6 +19,11 @@ class MyTestCase(unittest.TestCase):
     self.assertTrue(os.path.exists(f'{sys.prefix}/lib/python3.9/os.py'))
     self.assertTrue('listdir' in dir(os))
     self.assertTrue('os.py' in os.listdir(f'{sys.prefix}/lib/python3.9'))
+
+  def test_third_party_modules(self):
+    # pip install pandas
+    site_packages = f'{sys.prefix}/lib/python3.9/site-packages'
+    self.assertTrue('pandas' in os.listdir(site_packages))
 
 if __name__ == '__main__':
   unittest.main()
