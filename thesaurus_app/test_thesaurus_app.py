1e7672ba1082d83030a9fd2f4a2a871453b220f7
Jay T <jaytula@gmail.com>
2021-08-22 13:55:49 -0700

    95. Dealing with Case-Sensitive Words

--- thesaurus_app/test_thesaurus_app.py
+++ thesaurus_app/test_thesaurus_app.py
@@ -14,5 +14,8 @@ class ThesaurusAppTests(unittest.TestCase):
     actual = translate('rainn')
     self.assertEqual(actual, "The word doesn't. Please double check it.")
 
+    actual = translate('RAIN')
+    self.assertTrue(isinstance(actual, List) and actual[0].startswith('Precipitation'))
+
 if __name__ == '__main__':
   unittest.main()
--- thesaurus_app/thesaurus.py
+++ thesaurus_app/thesaurus.py
@@ -4,4 +4,5 @@ from typing import Dict, List
 data: Dict[str, List[str]] = json.load(open('./thesaurus_app/data.json', 'r'))
 
 def translate(s: str):
+  s = s.lower()
   return data[s] if s in data else "The word doesn't. Please double check it."
