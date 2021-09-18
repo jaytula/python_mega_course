8642c00902071e32edd44450e208ff085d84ea49
Jay T <jaytula@gmail.com>
2021-09-18 08:58:48 -0700

    121. Adding HTML on Popups

---
+++ mapping/adding_html_on_popups.py
@@ -0,0 +1,35 @@
+from branca.element import IFrame
+import folium
+import pandas
+
+map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Stamen Terrain")
+
+df = pandas.read_csv('Volcanoes_USA.txt')
+
+lat = list(df["LAT"])
+lon = list(df["LON"])
+elev = list(df["ELEV"])
+
+html = """<h4>Volcano inofrmation:</h4>
+Height: %s m"""
+
+# Adding some markers
+map.add_child(folium.Marker(location=[38.2, -99.1], popup="Hi I am a Marker", icon=folium.Icon(color='green')))
+
+# Using Feature Groups
+fg = folium.FeatureGroup(name="My Map")
+
+coordinates = [
+  [37.2, -93.1],
+  [36.2, -94.1],
+  [39.8, -92.3],
+  [40, -90]
+]
+
+for lt, ln, el in zip(lat, lon, elev):
+  iframe = folium.IFrame(html=html % str(el), width=200, height=100)
+  fg.add_child(folium.Marker(location=[lt, ln], popup=folium.Popup(iframe), icon=folium.Icon(color='red')))
+
+map.add_child(fg)
+
+map.save("Map1.html")
