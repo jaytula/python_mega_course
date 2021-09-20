5fd587ee8a01052624069a47f6ba8fe20fd9f459
Jay T <jaytula@gmail.com>
2021-09-20 08:11:07 -0700

    128. Styling the Population Layer

--- mapping/population_layer.py
+++ mapping/population_layer.py
@@ -40,7 +40,9 @@ for lt, ln, el in zip(lat, lon, elev):
   # Ultimately a successful search should lead to https://python-visualization.github.io/folium/modules.html#folium.vector_layers.path_options
   fg.add_child(folium.CircleMarker(location=[lt, ln], radius=6, color="grey", popup=folium.Popup(iframe), fill_color=color_producer(el), fill_opacity=0.7))
 
-fg.add_child(folium.GeoJson(data=(open('world.json', 'r', encoding='utf-8-sig').read())))
+fg.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read(),
+  style_function=lambda x: {'fillColor': 'green' if x['properties']['POP2005'] < 10000000 else 'orange' if x['properties']['POP2005'] < 20000000 else 'red'}
+))
 
 map.add_child(fg)
 
