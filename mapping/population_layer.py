8358469fd03e36f289c452002e9c906c64835b85
Jay T <jaytula@gmail.com>
2021-09-20 08:15:56 -0700

    129. Adding a Layer Control Panel

--- mapping/population_layer.py
+++ mapping/population_layer.py
@@ -25,7 +25,7 @@ Height: %s m"""
 map.add_child(folium.Marker(location=[38.2, -99.1], popup="Hi I am a Marker", icon=folium.Icon(color='green')))
 
 # Using Feature Groups
-fg = folium.FeatureGroup(name="My Map")
+fgv = folium.FeatureGroup(name="Volanoes")
 
 coordinates = [
   [37.2, -93.1],
@@ -38,12 +38,15 @@ for lt, ln, el in zip(lat, lon, elev):
   iframe = folium.IFrame(html=html % str(el), width=200, height=100)
   # Lecture 126 lacks explanation of how to discover the kwargs
   # Ultimately a successful search should lead to https://python-visualization.github.io/folium/modules.html#folium.vector_layers.path_options
-  fg.add_child(folium.CircleMarker(location=[lt, ln], radius=6, color="grey", popup=folium.Popup(iframe), fill_color=color_producer(el), fill_opacity=0.7))
+  fgv.add_child(folium.CircleMarker(location=[lt, ln], radius=6, color="grey", popup=folium.Popup(iframe), fill_color=color_producer(el), fill_opacity=0.7))
 
-fg.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read(),
+fgp = folium.FeatureGroup(name="Population")
+fgp.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read(),
   style_function=lambda x: {'fillColor': 'green' if x['properties']['POP2005'] < 10000000 else 'orange' if x['properties']['POP2005'] < 20000000 else 'red'}
 ))
 
-map.add_child(fg)
+map.add_child(fgv)
+map.add_child(fgp)
+map.add_child(folium.LayerControl())
 
 map.save("Map1.html")
