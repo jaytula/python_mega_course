51c7ab3f7e48e69a8acc52c8c70369c6f831be90
Jay T <jaytula@gmail.com>
2024-01-17 07:38:36 +0000

    Add day3/03-main.py

---
+++ day3/03-main.py
@@ -0,0 +1,19 @@
+todos = []
+
+while True:
+    user_action = input("Type add or show or exit: ")
+    user_action = user_action.strip()
+    match user_action:
+        case 'add':
+            todo = input("Enter a todo: ")
+            todos.append(todo)
+        case 'show' | 'display':
+            for item in todos:
+                item = item.title()
+                print(item)
+        case 'exit':
+            break
+        case _:
+            print("Hey, you entered an unknown command")
+
+print('Bye!')
